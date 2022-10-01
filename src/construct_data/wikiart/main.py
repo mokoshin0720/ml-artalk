d9789838953d219d6b0b4560e8fb12d72c2aef44
mokoshin0720 <shinya.work0720@gmail.com>
2022-10-01 16:52:41 +0900

    [add]get torch from art_style and filename

--- src/construct_data/wikiart/main.py
+++ src/construct_data/wikiart/main.py
@@ -2,14 +2,18 @@ import torch
 import torchvision
 from PIL import Image
 
-def get_torch(filename: str) -> torch.tensor:
-    img = Image.open(filename)
+def get_torch(art_style: str, filename: str) -> torch.tensor:
+    BASE_DIR = 'data/wikiart/'
+    dir = BASE_DIR + art_style + '/' + filename + '.jpg'
+
+    img = Image.open(dir)
     img_torch = torchvision.transforms.functional.to_tensor(img)
+
     return img_torch
 
 if __name__ == '__main__':
-    filename = "data/wikiart/Abstract_Expressionism/aaron-siskind_acolman-1-1955.jpg"
-    filename = "data/wikiart/Baroque/adriaen-brouwer_a-boor-asleep.jpg"
-    img_torch = get_torch(filename)
+    art_style = 'Baroque'
+    filename = 'adriaen-brouwer_a-boor-asleep'
+    img_torch = get_torch(art_style, filename)
     print(img_torch)
 
