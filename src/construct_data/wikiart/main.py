0e6dbeac4278f51704fb94e7cc5c2786c4e6d6af
mokoshin0720 <shinya.work0720@gmail.com>
2022-10-01 16:43:36 +0900

    [add]get torch image

--- src/construct_data/wikiart/main.py
+++ src/construct_data/wikiart/main.py
@@ -1,4 +1,10 @@
-import pandas as pd
+import torchvision
+from PIL import Image
 
 if __name__ == '__main__':
-    print('wikiart')
+    filename = "data/wikiart/Abstract_Expressionism/aaron-siskind_acolman-1-1955.jpg"
+    filename = "data/wikiart/Baroque/adriaen-brouwer_a-boor-asleep.jpg"
+    img = Image.open(filename)
+    img_torch = torchvision.transforms.functional.to_tensor(img)
+    print(img_torch)
+
