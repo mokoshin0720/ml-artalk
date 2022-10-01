f9e95884c33a4b9693067010911c946617fbfb43
mokoshin0720 <shinya.work0720@gmail.com>
2022-10-01 16:48:36 +0900

    [add]divide func

--- src/construct_data/wikiart/main.py
+++ src/construct_data/wikiart/main.py
@@ -1,10 +1,15 @@
+import torch
 import torchvision
 from PIL import Image
+
+def get_torch(filename: str) -> torch.tensor:
+    img = Image.open(filename)
+    img_torch = torchvision.transforms.functional.to_tensor(img)
+    return img_torch
 
 if __name__ == '__main__':
     filename = "data/wikiart/Abstract_Expressionism/aaron-siskind_acolman-1-1955.jpg"
     filename = "data/wikiart/Baroque/adriaen-brouwer_a-boor-asleep.jpg"
-    img = Image.open(filename)
-    img_torch = torchvision.transforms.functional.to_tensor(img)
+    img_torch = get_torch(filename)
     print(img_torch)
 
