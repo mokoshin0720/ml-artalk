af561ee102f2070a5bb0a6dcf04400663d0c77e5
mokoshin0720 <shinya.work0720@gmail.com>
2022-11-13 21:29:03 +0900

    [add]update attention

--- frontend/pages/demo.jsx
+++ frontend/pages/demo.jsx
@@ -3,14 +3,29 @@ import { useState } from "react";
 
 export default function Demo() {
     const demo_list = [
-        {src: '/adriaen-brouwer_feeling.jpg', attention_list: ['attentino1-1', 'attention1-2']},
-        {src: '/adriaen-van-ostade_smoker.jpg', attention_list: ['attentino2-1', 'attention2-2']},
-        {src: '/albert-bloch_piping-pierrot.jpg', attention_list: ['attentino3-1', 'attention3-2']},
-        {src: '/chuck-close_self-portrait-2000.jpg', attention_list: ['attentino4-1', 'attention4-2']},
-        {src: '/martiros-saryan_still-life-1913.jpg', attention_list: ['attentino5-1', 'attention5-2']},
+        {
+            src: '/adriaen-brouwer_feeling.jpg', 
+            attention_list: ['attentino1-1', 'attention1-2']
+        },
+        {
+            src: '/adriaen-van-ostade_smoker.jpg', 
+            attention_list: ['attentino2-1', 'attention2-2']
+        },
+        {
+            src: '/albert-bloch_piping-pierrot.jpg', 
+            attention_list: ['attentino3-1', 'attention3-2']
+        },
+        {
+            src: '/chuck-close_self-portrait-2000.jpg', 
+            attention_list: ['attentino4-1', 'attention4-2']
+        },
+        {
+            src: '/martiros-saryan_still-life-1913.jpg', 
+            attention_list: ['attentino5-1', 'attention5-2']
+        },
     ]
 
-    const [_, setSrc] = useState()
+    const [src, setSrc] = useState()
     const [attentionList, setAttentionList] = useState([])
     const [attention, setAttention] = useState()
 
@@ -19,15 +34,19 @@ export default function Demo() {
         setAttentionList(demo_list.find(value => value.src == e.target.value).attention_list)
     }
 
+    console.log(src)
+    console.log(attention)
+    console.log('=============================')
+
     return (
         <div>
             <h1 className='font-bold text-4xl text-center pb-10'>Demo</h1>
 
             <div>
                 <ul className='grid grid-cols-5 place-items-center pb-10'>
-                    {demo_list.map((demo) => {
+                    {demo_list.map((demo, idx) => {
                         return (
-                            <li>
+                            <li key={idx}>
                                 <Image src={demo.src} objectFit='contain' width={180} height={180} />
                                 <div className='text-center'>
                                     <input type='radio' name='radio-img' value={demo.src} className='mt-2' onChange={changeSrc} alt='tmp' />
@@ -55,14 +74,18 @@ export default function Demo() {
 }
 
 function AttentinoList(props) {
+    const changeAttention = e => {
+        props.setAttention(e.target.value)
+    }
+
     return (
         <div>
             <ul>
-            {props.attention_list.map((attention) => {
+            {props.attention_list.map((attention, idx) => {
                 return (
-                    <li>
-                        <input type='radio' className='hidden' name='radio-object' />
-                        <label className="flex flex-col w-full max-w-lg text-center border-2 rounded border-gray-900 p-2 my-1 text-xl hover:bg-blue-200">{attention}</label>
+                    <li key={idx}>
+                        <input type='radio' name='radio-object' value={attention} onChange={changeAttention} />
+                        <label>{attention}</label>
                     </li>
                 )
             })}
