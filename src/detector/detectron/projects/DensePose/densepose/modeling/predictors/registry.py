f3b3700322e1fbd3d440eb50c5e5801c70bf1fea
mokoshin0720 <shinya.work0720@gmail.com>
2022-12-09 16:09:06 +0900

    [add]third party

---
+++ src/detector/detectron/projects/DensePose/densepose/modeling/predictors/registry.py
@@ -0,0 +1,5 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from detector.detectron.detectron2.utils.registry import Registry
+
+DENSEPOSE_PREDICTOR_REGISTRY = Registry("DENSEPOSE_PREDICTOR")
